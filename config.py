9bfa74ca984dd80c8d6b79d4dbb12df0dbaa0a22
monty23monty <74265105+monty23monty@users.noreply.github.com>
2024-07-31 18:52:17 +0100

    Going to Dockerdas

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'uerguerhgiuoehgeurih'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://leo:leo@localhost:5432/MKLv0.1'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@db/postgres')
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
